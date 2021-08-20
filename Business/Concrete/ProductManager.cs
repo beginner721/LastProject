0affe9a1e62d8e394c380016522f9ff6c7e324bf
Baran Kurtbogan <barankurtbogan@gmail.com>
2021-08-20 12:26:50 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal; //injection
+        //interface kullandık çünkü inmemory de çalışıyoruz yarın entityframework de olabilir değişimlere açık bir yapıdayız.
+
+        public ProductManager(IProductDal productDal) //ctor 
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //işkodları vs vs. 
+            //yetkisi varsa vs. ardından en alttaki kod çalışır.
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //classları çıplak bırakmamak için bunu kullanıyoruz
+    //IEntity demek bu bir veritabanı nesnesidir demektir 
+    //IEntity implement eden class, bir veritabanı tablosudur ! (kişisel yorumdur.)
+    public interface IEntity
+    {
+    }
+}
